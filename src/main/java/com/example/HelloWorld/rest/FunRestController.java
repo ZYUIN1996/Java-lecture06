bc1d2099191dcda28238af9482eec96764dccdeb
ZYUIN1996 <133088892+ZYUIN1996@users.noreply.github.com>
2024-02-26 22:39:41 +0900

    Hello Worldを出力するコードを記載

--- src/main/java/com/example/HelloWorld/rest/FunRestController.java
+++ src/main/java/com/example/HelloWorld/rest/FunRestController.java
@@ -1,2 +1,15 @@
-package com.example.HelloWorld.rest;public class FunRestController {
+package com.example.HelloWorld.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // ルートURL(/)にアクセスがあったときに"Hello World"と返す
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
 }
